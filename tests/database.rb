7ae1ac97751f6b682161a38045732f29930779c4
Justin Davis <jrcd83@gmail.com>
2011-04-11 20:03:44 -0400

    Adds tests for database objects, only Alpm::DB::Local so far.

---
+++ tests/database.rb
@@ -0,0 +1,19 @@
+require 'test/unit'
+require 'alpm'
+
+class AlpmDBTest < Test::Unit::TestCase
+  def test_localdb
+    Alpm.root   = '/'
+    Alpm.dbpath = '/var/lib/pacman'
+    
+    localdb = Alpm.localdb
+    assert localdb, 'loaded localdb'
+    assert_equal 'local', localdb.name, 'name of local database is "local"'
+
+    %W{ url add_url update }.each do |m|
+      assert !localdb.respond_to?( m ), 'local dbs do not have #{m} method'
+    end
+
+    assert localdb.pkgs.length > 0, 'local database pkgcache seems to work'
+  end
+end
