2d36e2cf24e3c64e01a8bc485c3898f7f2fea29e
Justin Davis <jrcd83@gmail.com>
2011-04-12 20:46:15 -0400

    Fix log callback from using wrong symbols for loglevel.
    I forgot loglevel is a bitflag.

--- enum-symbols.h
+++ enum-symbols.h
@@ -10,7 +10,9 @@ extern ID * loglevel_symbols, * transflag_symbols, * transconv_symbols,
 #define PMERRNO_SYM( VAL )  ID2SYM( pmerrno_symbols[ VAL-1 ] )
 #define DEPMOD_SYM( VAL )   ID2SYM( depmod_symbols[ VAL-1 ] )
 #define REASON_SYM( VAL )   ID2SYM( pkgreason_symbols[ VAL ] )
-#define LOGLEVEL_SYM( VAL ) ID2SYM( loglevel_symbols[ VAL-1 ] )
+
+#define LOGLEVEL_SYM( VAL ) \
+    ID2SYM( loglevel_symbols[ bitflag_to_tblindex(VAL) ] )
 
 void define_enum_symbols ();
 int bitflag_to_tblindex ( int bitflag );
--- tests/callbacks.rb
+++ tests/callbacks.rb
@@ -10,9 +10,9 @@ class AlpmDBTest < Test::Unit::TestCase
     @evts = []
     Alpm.logcb { |lvl, msg| @evts << [ lvl, msg ] }
     Alpm.root = '/'
-    assert_event :function, %Q{option 'root' = /\n}
+    assert_event :debug, %Q{option 'root' = /\n}
     Alpm.dbpath = '/var/lib/pacman'
-    assert_event :function, %Q{option 'dbpath' = /var/lib/pacman/\n}
-    assert_event :function, %Q{option 'lockfile' = /var/lib/pacman/db.lck\n}
+    assert_event :debug, %Q{option 'dbpath' = /var/lib/pacman/\n}
+    assert_event :debug, %Q{option 'lockfile' = /var/lib/pacman/db.lck\n}
   end
 end
